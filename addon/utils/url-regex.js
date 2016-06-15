94f338c01e96430be89f95c597c83d045e40748c
Kamil Ejsymont <kamil.ejsymont@netguru.pl>
2016-06-15 14:18:50 +0200

    Change regex a little bit

--- addon/utils/url-regex.js
+++ addon/utils/url-regex.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 function urlRegex () {
-  return /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
+  return /((([A-Za-z]{3,9}:(?:\/\/)?)[A-Za-z0-9.-]+|(?:www.)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
 }
 
 // Shortens the URL and adds three dots to the end
