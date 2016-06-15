e59e10d1cfc18701c6e65f9031cee4aee31d00f3
Kamil Ejsymont <kamil.ejsymont@netguru.pl>
2016-06-15 14:08:48 +0200

    Change regex

--- addon/utils/url-regex.js
+++ addon/utils/url-regex.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 
-// URL regex courtesy of https://github.com/kevva/url-regex
 function urlRegex () {
   return /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
 }
